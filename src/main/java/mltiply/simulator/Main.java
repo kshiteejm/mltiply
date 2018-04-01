a68b3c4efb2e012c7e58007e99faea66f7a7afac
kshiteejm <kmahajan17@gmail.com>
2018-04-01 13:02:27 -0500

    slight update

--- src/main/java/mltiply/simulator/Main.java
+++ src/main/java/mltiply/simulator/Main.java
@@ -1,13 +1,10 @@
 package mltiply.simulator;
 
-/**
- * Hello world!
- *
- */
-public class Main 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+
 }
