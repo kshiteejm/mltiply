945ea7c69fce3d0dc33e92e07c0bc140f67bf747
kshiteejm <kmahajan17@gmail.com>
2019-02-25 18:22:57 -0600

    redesign from scratch

--- src/main/java/mltiply/events/eventhandlers/EventHandler.java
+++
@@ -1,7 +0,0 @@
-package mltiply.events.eventhandlers;
-
-import mltiply.events.simulator.Simulator;
-
-public interface EventHandler<E> {
-  public void handle(Simulator s, E eventObject, double time);
-}
--- src/main/java/mltiply/events/schedulers/IntraJobScheduler.java
+++
@@ -1,9 +0,0 @@
-package mltiply.events.schedulers;
-
-import mltiply.events.datastructures.Job;
-import mltiply.events.simulator.Simulator;
-
-public interface IntraJobScheduler {
-
-  public boolean assignTask(Simulator s, Job j, double time);
-}
